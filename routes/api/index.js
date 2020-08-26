4063b495fd6e7316384d9b5a002dbe851ce9edc2
MasonM <Mason.P.Marinko@gmail.com>
2020-08-25 20:38:30 -0600

    updated, now working on front end

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
 
+router.use('/comments', commentRoutes);
 //add prefix of `/pizzas` to routs created un `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
 
